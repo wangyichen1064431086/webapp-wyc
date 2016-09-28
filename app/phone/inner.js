252b544617be61873517667d4062cb0a793d7aa5
wangyichen1064431086 <1064431086@qq.com>
2016-09-28 22:51:53 +0800

    0928 do in home

---
+++ app/phone/inner.js
@@ -0,0 +1,6 @@
+// 启动页启动时，如果启动页的图标图片加载错误则用此文字填充
+function handleLogoError(){
+    document.getElementById("bookname").innerHTML='FINANCIAL TIMES';
+}
+//document.querySelector("#bookname img").onerror=handleLogoError;
+
